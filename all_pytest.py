d42321ea741e8184020168cff7e2c5b10f0a9b11
liufu123 <13265861735@163.com>
2020-09-11 22:27:37 +0800

    11111

--- all_pytest.py
+++ all_pytest.py
@@ -4,4 +4,5 @@ import pytest
 
 
 if __name__=="__main__":
-    pytest.main(["./case","--alluredir","./report/reportallure/"])
+    # pytest.main(["./case","--alluredir","./report/reportallure/"])
+    pytest.main(["./case"])
